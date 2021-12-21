27faea82bce9a552c8f3ea6b9b9b955ac1f23c3b
Aslan Mukhamedzhan <aslan.muhamedzhan33@gmail.com>
2021-12-21 13:20:46 +0600

    second

---
+++ src/components/HeroSection.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from './Button'
+import './HeroSection.css'
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            {/* <video src="videos/video-1.mp4" autoPlay loop muted/> */}
+            <h1>KAZAKHSTAN'S BEAUTIFUL PLACES</h1>
+            <p>So, what are you waiting for?</p>
+            <div className="hero-btns">
+                <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+                <Button className='btns' buttonSize='btn--large'>WATCH TRAILER <i className='far fa-play-circle'/></Button>
+            
+            </div>
+            
+
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import App from '../../App'
+import Cards from '../Cards'
+import Footer from '../Footer'
+import HeroSection from '../HeroSection'
+
+function Home() {
+    return (
+        <div>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
